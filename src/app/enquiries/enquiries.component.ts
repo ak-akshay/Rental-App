6329547875fe0797eb60d0507a69189a99a13b8d
ak-akshay <kak.akshay17@gmail.com>
2019-07-08 15:10:12 +0530

    Added delete enquiry functionality

--- src/app/enquiries/enquiries.component.ts
+++ src/app/enquiries/enquiries.component.ts
@@ -9,6 +9,9 @@ import { EnquiryService } from '../services/enquiry.service';
 export class EnquiriesComponent implements OnInit {
 
   enquiries=[]
+  enq:boolean=false
+  deleteMode:boolean=false
+  toDelete=[]
   constructor(public enquiryService:EnquiryService) { }
 
   ngOnInit() {
@@ -18,7 +21,37 @@ export class EnquiriesComponent implements OnInit {
   getEnquiries(){
     this.enquiryService.getEnquiries().subscribe(res=>{
       this.enquiries = res
+      if(this.enquiries.length!=0){
+        this.enq=true
+      }
       console.log(this.enquiries)
     })
+  }
+
+  checkboxChanged(event,value){
+    if(event.target.checked){
+     this.toDelete.push(value)
+     console.log(this.toDelete)
+    }
+    if(!event.target.checked){
+      let index = this.toDelete.indexOf(value);
+      if(index > -1)
+        {this.toDelete.splice(index,1)}
+      console.log(this.toDelete)
+    }
+  }
+
+  cancelDelete(){
+    this.toDelete=[]
+  }
+  
+  confirmDelete(){
+    let index= this.toDelete.length - 1
+    while(index > -1)
+    {
+      this.enquiryService.deleteEnquiry(this.toDelete[index])
+      index--;
+      console.log(this.toDelete)
+    }
   }
 }
--- src/app/services/enquiry.service.ts
+++ src/app/services/enquiry.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthService } from './auth.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,24 @@ export class EnquiryService {
   constructor(public db:AngularFirestore,public authService:AuthService) { }
  
   addEnquiry(enquiry){
-    return this.db.collection('enquiry').add(enquiry)
+    return this.db.collection('enquiries').add(enquiry)
   }
 
   getEnquiries(){
-    return this.db.collection('enquiry',ref=>ref.where('ownerEmail','==',this.authService.userDetails.email)).valueChanges()
+    return this.db.collection('enquiries',ref=>ref.where('ownerEmail','==',this.authService.userDetails.email)).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as any;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    ); 
+  }
+
+  deleteEnquiry(id){
+    this.db.collection('enquiries').doc(id).delete().then(() => {
+      console.log("Deleted")
+    }).catch(err => {
+      console.log(err)
+    })
   }
 }
